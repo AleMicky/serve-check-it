1bd747e8ee2e11f5dbcf6f5f92762291f166d5fd
AleMicky <miguel.ale19934@gmail.com>
2022-08-15 11:47:25 -0400

    prod heruko

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EncuestaModule } from './encuesta/encuesta.module';
 import { CommonModule } from './common/common.module';
- 
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -14,12 +14,16 @@ import { CommonModule } from './common/common.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      ssl: {rejectUnauthorized: false},
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
     EncuestaModule,
-    CommonModule, 
+    CommonModule,
   ],
   controllers: [],
   providers: [],
